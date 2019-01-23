be56263e4278409a5018398cba7274f3a90c7f85
Matthew Lovell <lovellmatt@rocketmail.com>
2019-01-23 18:02:55 -0500

    Added a Working Greeting using React

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 class NameForm extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
 
-      handleSubmit(event) {
-        alert('A name was submitted: '    this.state.value);
-        event.preventDefault();
-      }
-
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+  handleSubmit(event) {
+    let Name = this.state.value;
+    let alphabet = /^[A-Za-z]+$/;
+    if (Name.match(alphabet)) {
+      document.write("Hi User! Your Name is ",":" + " " + Name);
+      return true;
+    } else {
+      alert('Non-Ascii Character Used. Bad User');
+      return false;
     }
-     export default NameForm;
+  }
+
+  render() {
+    return (
+     <form onSubmit={ this.handleSubmit }>
+       <label>
+         Name:
+         <input type="text" value={this.state.value} onChange={this.handleChange} />
+       </label>
+       <input type="submit" value="Submit" />
+     </form>
+   );
+  }
+}
+export default NameForm;
