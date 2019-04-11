5f45ffd90ebb56ed3e17a1a5d3ae4ccf5c503030
285matt <39315718+285matt@users.noreply.github.com>
2019-04-10 20:17:11 -0400

    Add files via upload

---
+++ lab3/screens/dog.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet,
+    Image,
+    Text,
+    View,
+    } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  render() {
+    let title = this.props.navigation.getParam
+    ("Title", "Dog");
+    let image = this.props.navigation.getParam
+    ("Image", undefined);
+    return (
+      <View><Text>{title}</Text><Image source={image} style={{
+
+    resizeMode: 'cover', // or 'stretch',
+    justifyContent: 'center',
+  }}></Image></View>
+
+    );
+
+  }
+}
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  TextStyle: {
+    color: '#0250a3',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 10,
+  },
+});
