fc2b403d208bc9283ee1305da57951bbdf74eeff
285matt <39315718+285matt@users.noreply.github.com>
2019-04-29 11:26:38 -0400

    Add files via upload
    Updated to correct errors

--- lab4/LocationButton/index.js
+++ lab4/LocationButton/index.js
@@ -1,35 +1,39 @@
-import React, { Component } from "react";
+        import React, { Component } from "react";
 
-import Button from "./../Button";
-import styles from "./style.js";
+        import Button from "./../Button";
+        import styles from "./style.js";
 
-const style = { backgroundColor: "#DDDDDD" };
+        const style = { backgroundColor: "#DDDDDD" };
 
-class LocationButton extends Component {
-  _onPress() {
-    navigator.geolocation.getCurrentPosition(
-      initialPosition => {
-        this.props.onGetCoords(
-          initialPosition.coords.latitude,
-          initialPosition.coords.longitude
-        );
-      },
-      error => {
-        alert(error.message);
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  }
+        class LocationButton extends Component {
+          _onPress = () => {
+            navigator.geolocation.getCurrentPosition(
+              initialPosition => {
+                this.props.onGetCoords(
+                  initialPosition.coords.latitude,
+                  initialPosition.coords.longitude
+                );
+              },
+              error => {
+                alert(error.message);
+              },
+              { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+            );
+          }
 
-  render() {
-    return (
-      <Button
-        label="Use Current Location"
-        style={style}
-        onPress={this._onPress.bind(this)}
-      />
-    );
-  }
-}
+          componentDidMount() {
+            this._onPress()
+          }
 
-export default LocationButton;
+          render() {
+            return (
+              <Button
+                label="Use Current Location"
+                style={style}
+                onPress={this._onPress.bind(this)}
+              />
+            );
+          }
+        }
+
+        export default LocationButton;
